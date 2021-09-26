0a20f9b653b2ff63295fc63edf999b544cb7b9f2
Lakshan-Banneheke <lakshan.18@cse.mrt.ac.lk>
2021-09-26 23:59:27 +0530

    import shit

--- src/redux/sagas/serviceSaga.js
+++ src/redux/sagas/serviceSaga.js
@@ -1,10 +1,10 @@
-export function login (creds) {
-  axios.post(url, creds)
-}
-
-export function register (user) {
-  console.log(user)
-}
+// export function login (creds) {
+//   axios.post(url, creds)
+// }
+//
+// export function register (user) {
+//   console.log(user)
+// }
 
 ///user/login
 //user/register
