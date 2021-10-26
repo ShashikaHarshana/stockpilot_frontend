fe8e0e99fb55e4e1c871af8dffb140ee94200278
MeelanBandara <64338195+Meelan-98@users.noreply.github.com>
2021-10-26 13:31:45 +0530

    Profile updated

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,14 +1,40 @@
 import React, { useEffect } from 'react'
 import NavBar from '../components/NavBar'
+import VerticalTabs from '../components/TabPanel'
+import ImageAvatars from '../components/ImageAvatars'
+import CenteredGrid from '../components/CenteredGrid'
+import { makeStyles } from '@material-ui/core'
+
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(3, 2),
+    height: 200,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center"
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 const Profile = () => {
   const handleClick = () => {}
-
+  const classes = useStyles();
   return (
     <div>
-      <NavBar />
-      <h2>This is profile</h2>
+      <div>
+        <NavBar />
+      </div>
+      <div style={{marginTop: '125px'}}>
+        <CenteredGrid />
+      </div>
     </div>
+  
   )
 }
 
