37725088b7c1fae4a1c6c3439cacbab0c6681eff
Lakshan <lakshan.18@cse.mrt.ac.lk>
2021-11-16 19:06:04 +0530

    Removed notifs/firebase for mobile

--- src/redux/sagas/rootSaga.js
+++ src/redux/sagas/rootSaga.js
@@ -3,12 +3,10 @@ import { watchWatchlist } from './watchlistSaga'
 import { all } from 'redux-saga/effects'
 import { watchInit } from './chartSaga'
 
-
 export function * watcherSaga () {
   yield all([
     watchAuth(),
     watchWatchlist(),
-    watchInit(),
-
+    watchInit()
   ])
 }
