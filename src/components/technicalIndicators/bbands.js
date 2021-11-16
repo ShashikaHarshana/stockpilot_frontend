c11afc79195be058eaa78804e0d714971a27c9e1
Lakshan <lakshan.18@cse.mrt.ac.lk>
2021-11-16 12:19:48 +0530

    bug fix

--- src/components/technicalIndicators/bbands.js
+++ src/components/technicalIndicators/bbands.js
@@ -22,53 +22,55 @@ let getBBands = (
   fetch(url)
     .then(res => res.json())
     .then(data => {
-      let tempUpper = []
-      let tempMiddle = []
-      let tempLower = []
+        if (!data.hasOwnProperty('error')) {
+            let tempUpper = []
+            let tempMiddle = []
+            let tempLower = []
 
-      let dataUpper = data['upperband']
-      let dataMiddle = data['middleband']
-      let dataLower = data['lowerband']
+            let dataUpper = data['upperband']
+            let dataMiddle = data['middleband']
+            let dataLower = data['lowerband']
 
-      for (let key in dataUpper) {
-        if (dataUpper.hasOwnProperty(key)) {
-          if (dataUpper.hasOwnProperty(key)) {
-            let object = {
-              time: key / 1000,
-              value: dataUpper[key]
+            for (let key in dataUpper) {
+                if (dataUpper.hasOwnProperty(key)) {
+                    if (dataUpper.hasOwnProperty(key)) {
+                        let object = {
+                            time: key / 1000,
+                            value: dataUpper[key]
+                        }
+                        tempUpper.push(object)
+                    }
+                    if (dataMiddle.hasOwnProperty(key)) {
+                        let object = {
+                            time: key / 1000,
+                            value: dataMiddle[key]
+                        }
+                        tempMiddle.push(object)
+                    }
+                    if (dataLower.hasOwnProperty(key)) {
+                        let object = {
+                            time: key / 1000,
+                            value: dataLower[key]
+                        }
+                        tempLower.push(object)
+                    }
+                }
             }
-            tempUpper.push(object)
-          }
-          if (dataMiddle.hasOwnProperty(key)) {
-            let object = {
-              time: key / 1000,
-              value: dataMiddle[key]
-            }
-            tempMiddle.push(object)
-          }
-          if (dataLower.hasOwnProperty(key)) {
-            let object = {
-              time: key / 1000,
-              value: dataLower[key]
-            }
-            tempLower.push(object)
-          }
+            bbandUpper.setData(tempUpper)
+            bbandMiddle.setData(tempMiddle)
+            bbandLower.setData(tempLower)
+            console.log('update chart data')
+            dispatch(
+                updateInternalIndicatorData({
+                    type: 'bbands',
+                    data: {
+                        upper: [...tempUpper, ...lineData.upper],
+                        middle: [...tempMiddle, ...lineData.middle],
+                        lower: [...tempLower, ...lineData.lower]
+                    }
+                })
+            )
         }
-      }
-      bbandUpper.setData(tempUpper)
-      bbandMiddle.setData(tempMiddle)
-      bbandLower.setData(tempLower)
-      console.log('update chart data')
-      dispatch(
-        updateInternalIndicatorData({
-          type: 'bbands',
-          data: {
-            upper: [...tempUpper, ...lineData.upper],
-            middle: [...tempMiddle, ...lineData.middle],
-            lower: [...tempLower, ...lineData.lower]
-          }
-        })
-      )
     })
     .catch()
 }
